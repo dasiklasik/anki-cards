b37e4656f1dea7c67f06b2f5494397af11fc798b
dasiklasik <daryasamsonowicz@gmail.com>
2023-06-10 18:12:35 +0300

    refactor: delete antd and refactor with prettier

--- src/trash/features/counter/Counter.tsx
+++ src/trash/features/counter/Counter.tsx
@@ -21,19 +21,11 @@ export function Counter() {
   return (
     <div>
       <div className={styles.row}>
-        <button
-          className={styles.button}
-          aria-label="Decrement value"
-          onClick={() => dispatch(decrement())}
-        >
+        <button className={styles.button} aria-label="Decrement value" onClick={() => dispatch(decrement())}>
           -
         </button>
         <span className={styles.value}>{count}</span>
-        <button
-          className={styles.button}
-          aria-label="Increment value"
-          onClick={() => dispatch(increment())}
-        >
+        <button className={styles.button} aria-label="Increment value" onClick={() => dispatch(increment())}>
           +
         </button>
       </div>
@@ -44,16 +36,10 @@ export function Counter() {
           value={incrementAmount}
           onChange={(e) => setIncrementAmount(e.target.value)}
         />
-        <button
-          className={styles.button}
-          onClick={() => dispatch(incrementByAmount(incrementValue))}
-        >
+        <button className={styles.button} onClick={() => dispatch(incrementByAmount(incrementValue))}>
           Add Amount
         </button>
-        <button
-          className={styles.asyncButton}
-          onClick={() => dispatch(incrementAsync(incrementValue))}
-        >
+        <button className={styles.asyncButton} onClick={() => dispatch(incrementAsync(incrementValue))}>
           Add Async
         </button>
         <button className={styles.button} onClick={() => dispatch(incrementIfOdd(incrementValue))}>
